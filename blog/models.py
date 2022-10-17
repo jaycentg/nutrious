7e0a2dcb5f56597fa226139d716dc4d6797d9ada
Dhafin Raditya Juliawan <dhafinraditya35@gmail.com>
2022-10-17 22:24:45 +0700

    Finishing functionality of blog trivia 50%

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+
+from home.models import AppUser
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(AppUser, on_delete= models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
