88d04d66caac496bf90ed5e1f4fd9f48a40a21d5
Stelline Claudia <stellineclaudia32@gmail.com>
2022-10-23 22:39:43 +0700

    menyambungkan home dengan calorietracker dan memperbaiki beberapa kesalahan

--- calorietracker/models.py
+++ calorietracker/models.py
@@ -5,5 +5,5 @@ from home.models import AppUser
 
 class CalorieTracker(models.Model):
     calorie = models.IntegerField()
-    description = models.TimeField()
+    description = models.TextField()
     user =  models.ForeignKey(AppUser, on_delete= models.CASCADE)
