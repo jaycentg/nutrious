0f5e9290aeae19a283469f11626c60a69a91074f
Jaycent Gunawan Ongris <jaycent.gunawan@ui.ac.id>
2022-10-26 19:57:44 +0700

    add message feature

--- home/admin.py
+++ home/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from home.forms import *
+from home.models import *
 
 # Register your models here.
-from .forms import AppUserCreationForm, AppUserChangeForm
-from .models import AppUser
-
 class AppUserAdmin(UserAdmin):
     add_form = AppUserCreationForm
     form = AppUserChangeForm
     model = AppUser
     list_display = ['username', 'nickname', 'description', 'profile_pict_url']
     # Untuk menyetel user yang bisa menjadi admin
-    fieldsets = UserAdmin.fieldsets + (
-            (None, {'fields': ('nickname', 'description', 'profile_pict_url', 'is_user', 'is_verified_user', 'is_admin')}),
+    fieldsets = ((None, {'fields': ('nickname', 'description', 'profile_pict_url', 'is_user', 'is_verified_user', 'is_admin')}),
     )
 
+class MessageAdmin(admin.ModelAdmin):
+    form = MessageForm
+    list_display = ['get_user', 'message', 'time_sent']
 
 admin.site.register(AppUser, AppUserAdmin)
+admin.site.register(Message, MessageAdmin)
