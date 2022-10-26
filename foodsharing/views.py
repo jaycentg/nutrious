6db65da8591a22b649c11c3a096ba74dafe680d1
annavaws <annavaws@gmail.com>
2022-10-26 11:34:18 +0700

    using modal

--- foodsharing/views.py
+++ foodsharing/views.py
@@ -1,6 +1,6 @@
 import datetime
 from django.shortcuts import render, redirect
-from foodsharing.models import Post
+from foodsharing.models import Sharing
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.core import serializers
@@ -9,32 +9,33 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 # semua orang juga bisa baca
 def show_location(request):
-    data_post = Post.objects.order_by('-date')
+    data_post = Sharing.objects.order_by('-date')
     context = {
-        'postlist' : data_post
+        'locationlist' : data_post
     }
     return render(request, "location_page.html", context)
 
-def post_detail(request, id):
-    post_detail = Post.objects.get(pk = id)
+def location_detail(request, id):
+    location_detail = Sharing.objects.get(pk = id)
     context = {
-        'postdetail' : post_detail
+        'locationdetail' : location_detail
     }
     return render(request, "location_detail.html", context)
 
 # harus pas kondisi login
-@login_required(login_url='/home/login/')
+@login_required(login_url='/login/')
+@csrf_exempt
 def add_location(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
+        # title = request.POST.get('title')
+        description = request.POST.get('description')
         location = request.POST.get('location')
 
-        Post.objects.create(author=request.user, location = location, content=content, date=datetime.datetime.now())
+        Sharing.objects.create(author=request.user, location = location, description=description, date=datetime.datetime.now())
         return redirect('foodsharing:show_location')
 
-    return render(request, 'add_location.html')
+    # return render(request, 'add_location.html')
 
 def show_json(request):
-    data = Post.objects.all()
+    data = Sharing.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
