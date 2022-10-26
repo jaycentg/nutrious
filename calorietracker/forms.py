ea8d83d59558640bb1588fbd0e474003fad1643d
Stelline Claudia <stellineclaudia32@gmail.com>
2022-10-26 20:30:33 +0700

    menambah fitur reduce dan mengubah halaman html

--- calorietracker/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import CalorieTracker
-class Form(forms.ModelForm):
-    class Meta:
-        model = CalorieTracker
-        fields = ["calorie", "description",]
-        labels = {'calorie': "Kalori", 
-        "description": "Deskripsi",
-        }
--- calorietracker/views.py
+++ calorietracker/views.py
@@ -1,41 +1,46 @@
+
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from calorietracker.models import CalorieTracker
-from calorietracker.forms import Form
 from django.urls import reverse
 from django.core import serializers
+from calorietracker.models import Calorie
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+import datetime
 def show_caloriepage(request):
-    data = CalorieTracker.objects.filter(user = request.user.id)
-    calorie = data.only("calorie")
-
-    
-    total = 0
-    for i in calorie:
-        total= total +calorie[i]
-    context = {
-        'totalCalories' : total,
-        'data': data,
-         'user':request.user,
-    }
-    return render(request, "calorietracker.html", context)
+	
+	context = {
+    	'user' : request.user,
+	}
+	return render(request, 'calorietracker.html',context)
 
 @login_required(login_url='/home/login/')
 @csrf_exempt
 def add_calorie(request):
-    if request.method == "POST":
-        calorie = request.POST.get("calorie")
-        description = request.POST.get("description")
-        
-        CalorieTracker.objects.create(calorie = calorie, description = description, user=request.user)
-        return HttpResponse()
-    else:
-        return redirect('calorietracker:show_caloriepage')
+	if request.method == "POST":
+		calorie = request.POST.get("calorie")
+		description = request.POST.get("description")
+		category = request.POST.get("category")
+		Calorie.objects.create(category = category,is_increasing = True, date = datetime.datetime.now(),calorie= calorie, description = description, user=request.user)
+		return HttpResponse()
+	else:
+		return redirect('calorietracker:show_caloriepage')
+
+@login_required(login_url='/home/login/')
+@csrf_exempt
+def reduce_calorie(request):
+	if request.method == "POST":
+		calorie = request.POST.get("calorie")
+		description = request.POST.get("description")
+		category = request.POST.get("category")
+		Calorie.objects.create(category = category,is_increasing = False, date = datetime.datetime.now(), calorie = calorie, description = description, user=request.user)
+		return HttpResponse()
+	else:
+		return redirect('calorietracker:show_caloriepage')
+
 def show_json(request):
-    data =CalorieTracker.objects.filter(user=request.user.id)
+    data =Calorie.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")   
 
