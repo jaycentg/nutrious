d0eed064b5d84ab54c071f4527e15958be24bf9c
annavaws <annavaws@gmail.com>
2022-10-27 21:40:21 +0700

    using card

--- foodsharing/urls.py
+++ foodsharing/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from foodsharing.views import show_location, location_detail, add_location, show_json
-
+from foodsharing.views import delete, show_location, location_detail, add_location, show_json
+from foodsharing.views import edit_add, edit_add_save, delete
 app_name = 'foodsharing'
 
 urlpatterns = [
@@ -8,5 +8,7 @@ urlpatterns = [
     path('details/<int:id>', location_detail, name='location_detail'),
     path('add_location/', add_location, name='add_location'),
     path('json/', show_json, name='show_json'),
-    
+    path('edit_add/<int:id>', edit_add, name='edit_add'),
+    path('edit_add_save/<int:id>', edit_add_save, name='edit_add_save'),
+    path('delete/<int:id>', delete, name='delete'),
 ]
