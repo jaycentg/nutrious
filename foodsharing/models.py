2813ca32facc66dbdbc2c0c3f178cb02c002dc6b
annavaws <annavaws@gmail.com>
2022-10-28 17:23:54 +0700

    add models url to img

--- foodsharing/models.py
+++ foodsharing/models.py
@@ -8,6 +8,7 @@ class Sharing(models.Model):
     location = models.CharField(max_length=300)
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    img = models.URLField(default = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fartsmidnorthcoast.com%2Flisting%2Fa-device-for-orienting-oneself-at-the-centre-of-the-universe%2Fno-image-available-icon-6%2F&psig=AOvVaw2-dCd7UtB_XHJmMsY7WB88&ust=1666987680917000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCMj01ZabgfsCFQAAAAAdAAAAABAE")
 
     class Meta:
         # descending order
