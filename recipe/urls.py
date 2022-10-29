e2666cb98c75ac59b48157ec9c96d55769f044f1
farisumarr <muhammadfarisumarrahman@gmail.com>
2022-10-29 08:10:08 +0700

    Fix username & formatted date display

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from recipe.views import show_recipe, add_recipe, show_json
+from recipe.views import show_recipe, add_recipe, show_json, delete
 
 app_name = 'recipe'
 
 urlpatterns = [
     path('', show_recipe, name='show_recipe'),
     path('add/', add_recipe, name='add_recipe'),
+    path('delete/<int:id>', delete, name='delete'),
     path('json/', show_json, name='show_json'),
 ]
