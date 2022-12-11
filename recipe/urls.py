f05ed34aadc538a080d012c00ead7b4a716d9101
farisumarr <muhammadfarisumarrahman@gmail.com>
2022-12-11 08:52:37 +0700

    add function flutter

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from recipe.views import show_recipe, add_recipe, show_json, delete
+from recipe.views import show_recipe, add_recipe, show_json, delete, add_recipe_flutter
 
 app_name = 'recipe'
 
 urlpatterns = [
     path('', show_recipe, name='show_recipe'),
     path('add/', add_recipe, name='add_recipe'),
+    path('add-flutter/', add_recipe_flutter, name='add_recipe_flutter'),
     path('delete/<int:id>', delete, name='delete'),
     path('json/', show_json, name='show_json'),
 ]
